0db2ede9ed606ce7ea5dd26bd112a1ff83f64e49
franciscoj <frankmazta@gmail.com>
2024-05-25 18:08:13 -0400

    uso Promesas

--- programacion-asincrona/asincrona.js
+++ programacion-asincrona/asincrona.js
@@ -53,17 +53,41 @@
 //   console.log("Fin");
 // })();
 
-/*  uso callback */
-function cuadradoCallback(value, callback){
-  setTimeout(()=>{
-    callback(value, value * value);
-  },0| Math.random() * 1000);
-}
+/**************  uso callback **********************/
+// function cuadradoCallback(value, callback){
+//   setTimeout(()=>{
+//     callback(value, value * value);
+//   },0| Math.random() * 1000);
+// }
 
-cuadradoCallback(1, (value, result)=>{  
-  console.log("INICIA CALLBACK");
-  console.log(`El cuadrado de ${value} es ${result}`);
-  cuadradoCallback(2, (value, result)=>{  
-  console.log(`El cuadrado de ${value} es ${result}`);
+// cuadradoCallback(1, (value, result)=>{  
+//   console.log("INICIA CALLBACK");
+//   console.log(`El cuadrado de ${value} es ${result}`);
+//   cuadradoCallback(2, (value, result)=>{  
+//   console.log(`El cuadrado de ${value} es ${result}`);
+//   });
+// });
+
+/************** uso Promesas ****************/
+function cuadradoPromise(value){
+  if(typeof value !== "number") return Promise.reject(`Error, el valor "${value}" ingresado no es un numero`);
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve({
+        value,
+        result: value * value
+      });
+    }, 0 | Math.random() * 1000);
   });
-});
+}
+cuadradoPromise(1)
+.then(obj=>{
+  console.log("inico promise")
+  console.log(`El cuadrado de ${obj.value} es ${obj.result}`);
+  return cuadradoPromise(2);
+})
+.then(obj=>{
+  console.log(`El cuadrado de ${obj.value} es ${obj.result}`);
+  console.log("Fin promise")
+})
+.catch(err => console.error(err));
