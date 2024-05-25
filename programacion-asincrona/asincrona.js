1937de3ffd2366c7c9585c5abbed0ddb633ef0c4
franciscoj <frankmazta@gmail.com>
2024-05-24 22:04:19 -0400

    uso de callback

--- programacion-asincrona/asincrona.js
+++ programacion-asincrona/asincrona.js
@@ -14,45 +14,56 @@
 //   console.log(new Date().toLocaleTimeString())
 // },5000)
 
-
 /*  Codigo Sincrono Bloqueante */
-(()=>{
-  console.log("Codigo sincrono");
-  console.log("Inicio");
+// (() => {
+//   console.log("Codigo sincrono");
+//   console.log("Inicio");
 
-  function dos(){
-    console.log("dos");
-  }
-  function uno(){
-    console.log("uno");
-    dos();
-    console.log("tres");
-  }
-  uno();
-  console.log("Fin");
-})();
+//   function dos() {
+//     console.log("dos");
+//   }
+//   function uno() {
+//     console.log("uno");
+//     dos();
+//     console.log("tres");
+//   }
+//   uno();
+//   console.log("Fin");
+// })();
 
-/*  Codigo Asincrono no Bloqueante */
+// /*  Codigo Asincrono no Bloqueante */
 
-(()=>{
-  console.log("Codigo asincrono");
-  console.log("Inicio");
+// (() => {
+//   console.log("Codigo asincrono");
+//   console.log("Inicio");
 
-  function dos(){
-    setTimeout(function(){
-      console.log("dos");
-    },1000)
-  }
-  function uno(){
-    setTimeout(function(){
-      console.log("uno");
-    },0);
-    dos();
-    console.log("tres");
+//   function dos() {
+//     setTimeout(function () {
+//       console.log("dos");
+//     }, 1000);
+//   }
+//   function uno() {
+//     setTimeout(function () {
+//       console.log("uno");
+//     }, 0);
+//     dos();
+//     console.log("tres");
+//   }
+//   uno();
+//   console.log("Fin");
+// })();
 
-  }
-uno();
-console.log("Fin");
+/*  uso callback */
+function cuadradoCallback(value, callback){
+  setTimeout(()=>{
+    callback(value, value * value);
+  },0| Math.random() * 1000);
+}
 
-
-})();
+cuadradoCallback(1, (value, result)=>{  
+  console.log("INICIA CALLBACK");
+  console.log(`El cuadrado de ${value} es ${result}`);
+  cuadradoCallback(2, (value, result)=>{  
+  console.log(`El cuadrado de ${value} es ${result}`);
+  });
+});
