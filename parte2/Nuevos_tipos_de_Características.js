396075fbcc6689568dbd04599588743e24d429ed
franciscoj <frankmazta@gmail.com>
2024-05-31 23:37:57 -0400

    uso de map

--- parte2/Nuevos_tipos_de_Características.js
+++ parte2/Nuevos_tipos_de_Características.js
@@ -29,35 +29,56 @@
 //   console.log(propiedad);
 //   console.log(persona[propiedad])
 // }
-/************* uso de symbols ****************/
+/************* uso de symbol ⬆️****************/
 
 
-const set = new Set([1,2,3,3,4,5,true,false,false,{},{},"hola", "HOLA"]);
-console.log(set);
-console.log(set.size);
+// const set = new Set([1,2,3,3,4,5,true,false,false,{},{},"hola", "HOLA"]);
+// console.log(set);
+// console.log(set.size);
 
-const set2 = new Set();
-set2.add(1);
-set2.add(2);
-set2.add(2);
-set2.add(3);
-set2.add(true);
-set2.add(false);
-set2.add(false);
-set2.add("hola");
-set2.add("HOLA");
-console.log(set2);
+// const set2 = new Set();
+// set2.add(1);
+// set2.add(2);
+// set2.add(2);
+// set2.add(3);
+// set2.add(true);
+// set2.add(false);
+// set2.add(false);
+// set2.add("hola");
+// set2.add("HOLA");
+// console.log(set2);
 
-for(item of set){
-  console.log(item);
-}
-set2.forEach(item => console.log((item)));
+// for(item of set){
+//   console.log(item);
+// }
+// set2.forEach(item => console.log((item)));
 
-let arr = Array.from(set);
-console.log(arr);
+// let arr = Array.from(set);
+// console.log(arr);
 
-console.log(set.has("hola"));
+// console.log(set.has("hola"));
 
-/********* uso de Sets ***********/
+/********* uso de Set ⬆️***********/
+
+// let mapa = new Map();
+// mapa.set("nombre", "frank");
+// mapa.set("edad", 30);
+// mapa.set("apellido", "perez");
+
+// console.log(mapa);
+// console.log(mapa.size);
+// console.log(mapa.get("nombre"))
+// console.log(mapa.has("apellido"));
+// mapa.delete("edad")
+// console.log(mapa)
+
+// for (let [key, value] of mapa){
+//   console.log(`clave: ${key} valor: ${value}`);
+// }
+
+/**************uso del map ⬆️*************/
+
+
+/***********  **********/  
 
 
