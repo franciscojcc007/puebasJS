f0ab7fa19203ab3030225bc8d2052bfe72130c84
franciscoj <frankmazta@gmail.com>
2024-06-07 10:39:04 -0400

    Elementos del Documento

---
+++ parte2/Dom.js
@@ -0,0 +1,26 @@
+// console.log(window.document);
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.doctype);
+// console.log(document.charset);
+// console.log(document.title);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.styleSheets);
+// console.log(document.scripts);
+// setTimeout(() => {
+//   console.log(document.getSelection().toString());
+// }, 2000);
+
+
+//let texto = "Hola son fracisco soy developer Full Stack"
+
+// const saludar = (texto)=> speechSynthesis
+// .speak(new SpeechSynthesisUtterance(texto));
+// saludar(texto);
+
+
+/*Elementos del Documento⬆️*/
