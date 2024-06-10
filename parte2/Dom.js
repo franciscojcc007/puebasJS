1ef278b80b3eacdfe6b8a2a2c8743f43224310c2
franciscoj <frankmazta@gmail.com>
2024-06-10 11:58:08 -0400

    DOM Traversing - Recorriendo el DOM

--- parte2/Dom.js
+++ parte2/Dom.js
@@ -157,4 +157,19 @@
 
 // /******Texto y HTML⬆️******/
 
+// const $cards = document.querySelector(".cards");
+
+// console.log($cards);
+// console.log($cards.children);
+// console.log($cards.children[2]);
+// console.log($cards.parentElement);
+// console.log($cards.firstElementChild);
+// console.log($cards.lastElementChild);
+// console.log($cards.previousElementSibling);
+// console.log($cards.nextElementSibling);
+// console.log($cards.closest("div"));
+// console.log($cards.closest("body"));
+// console.log($cards.children[3].closest("section"));
+/******DOM Traversing - Recorriendo el DOM⬆️******/
+
 
