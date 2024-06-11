a5e6d7018e85d927cbde8b80b45fe2e3d5751e6a
franciscoj <frankmazta@gmail.com>
2024-06-11 19:37:52 -0400

    mejoras

--- parte2/Dom.js
+++ parte2/Dom.js
@@ -259,41 +259,43 @@
 
 /******Creando Elementos y Fragmentos⬆️******/
 
-const $cards = document.querySelector(".cards"),
-  $template = document.getElementById("template-card").content,
-  $fragment = document.createDocumentFragment(),
-  cardsContent = [
-    {
-      title: "Tecnología",
-      img: "https://placeimg.com/200/200/tech",
-    },
-    {
-      title: "Animales",
-      img: "https://placeimg.com/200/200/animals",
-    },
-    {
-      title: "Arquitectura",
-      img: "https://placeimg.com/200/200/arch",
-    },
-    {
-      title: "Gente",
-      img: "https://placeimg.com/200/200/people",
-    },
-    {
-      title: "Naturaleza",
-      img: "https://placeimg.com/200/200/nature",
-    },
-  ];
+// const $cards = document.querySelector(".cards"),
+//   $template = document.getElementById("template-card").content,
+//   $fragment = document.createDocumentFragment(),
+//   cardsContent = [
+//     {
+//       title: "Tecnología",
+//       img: "https://placeimg.com/200/200/tech",
+//     },
+//     {
+//       title: "Animales",
+//       img: "https://placeimg.com/200/200/animals",
+//     },
+//     {
+//       title: "Arquitectura",
+//       img: "https://placeimg.com/200/200/arch",
+//     },
+//     {
+//       title: "Gente",
+//       img: "https://placeimg.com/200/200/people",
+//     },
+//     {
+//       title: "Naturaleza",
+//       img: "https://placeimg.com/200/200/nature",
+//     },
+//   ];
 
-cardsContent.forEach((el) => {
-  $template.querySelector("img").setAttribute("src", el.img);
-  $template.querySelector("img").setAttribute("alt", el.title);
-  $template.querySelector("figcaption").textContent = el.title;
+// cardsContent.forEach((el) => {
+//   $template.querySelector("img").setAttribute("src", el.img);
+//   $template.querySelector("img").setAttribute("alt", el.title);
+//   $template.querySelector("figcaption").textContent = el.title;
 
-  let $clone = document.importNode($template, true);
-  $fragment.appendChild($clone);
-});
+//   let $clone = document.importNode($template, true);
+//   $fragment.appendChild($clone);
+// });
 
-$cards.appendChild($fragment);
-
+// $cards.appendChild($fragment);
 /******Templates HTML⬆️******/
+
+
+
