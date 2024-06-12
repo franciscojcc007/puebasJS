35dd6631b84c93a12120a8baaae758d495c862c4
franciscoj <frankmazta@gmail.com>
2024-06-11 21:56:58 -0400

    Modificando Elementos (Cool Style)

--- parte2/Dom.js
+++ parte2/Dom.js
@@ -297,21 +297,50 @@
 // $cards.appendChild($fragment);
 /******Templates HTML⬆️******/
 
-const $cards = document.querySelector(".cards"),
-  $newCard = document.createElement("figure"),
-  $cloneCards = $cards.cloneNode(true);
+// const $cards = document.querySelector(".cards"),
+//   $newCard = document.createElement("figure"),
+//   $cloneCards = $cards.cloneNode(true);
 
-$newCard.innerHTML = `
-  <img src="https://placeimg.com/200/200/any" alt="Any">
-  <figcaption>Any</figcaption>
-`;
-$newCard.classList.add("card");
+// $newCard.innerHTML = `
+//   <img src="https://placeimg.com/200/200/any" alt="Any">
+//   <figcaption>Any</figcaption>
+// `;
+// $newCard.classList.add("card");
 
 //$cards.replaceChild($newCard, $cards.children[2]);
 //$cards.removeChild($cards.lastElementChild);
-$cards.insertBefore($newCard, $cards.firstElementChild);
-document.body.appendChild($cloneCards);
+// $cards.insertBefore($newCard, $cards.firstElementChild);
+// document.body.appendChild($cloneCards);
 
 /******Modificando Elementos (Old Style)⬆️******/
+/*
+.insertAdjacent...
+  .insertAdjacentElement(position, el)
+  .insertAdjacentHTML(position, html)
+  .insertAdjacentText(position, text)
 
+Posiciones:
+  beforebegin(hermano anterior)
+  afterbegin(primer hijo)
+  beforeend(ultimo hijo)
+  afterend(hermano siguiente)
+*/
+
+const $cards = document.querySelector(".cards"),
+  $newCard = document.createElement("figure");
+
+let $contenCard = `
+  <img src="https://placeimg.com/200/200/any" alt="Any">
+  <figcaption></figcaption>
+`;
+$newCard.classList.add("card");
+
+$newCard.insertAdjacentHTML("afterbegin", $contenCard);
+$cards.insertAdjacentElement("beforeend", $newCard);
+$newCard.querySelector("figcaption").insertAdjacentText("afterbegin", "Any");
+
+//$cards.prepend($newCard);
+//$cards.append($newCard);
+//$cards.before($newCard);
+//$cards.after($newCard);
 /******Modificando Elementos (Cool Style)⬆️******/
