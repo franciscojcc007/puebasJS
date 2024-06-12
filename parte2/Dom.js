d76d2aa684736022451e8daaa8617851b06776ef
franciscoj <frankmazta@gmail.com>
2024-06-11 21:43:01 -0400

    Modificando Elementos (Old Style)

--- parte2/Dom.js
+++ parte2/Dom.js
@@ -297,5 +297,21 @@
 // $cards.appendChild($fragment);
 /******Templates HTML⬆️******/
 
+const $cards = document.querySelector(".cards"),
+  $newCard = document.createElement("figure"),
+  $cloneCards = $cards.cloneNode(true);
 
+$newCard.innerHTML = `
+  <img src="https://placeimg.com/200/200/any" alt="Any">
+  <figcaption>Any</figcaption>
+`;
+$newCard.classList.add("card");
 
+//$cards.replaceChild($newCard, $cards.children[2]);
+//$cards.removeChild($cards.lastElementChild);
+$cards.insertBefore($newCard, $cards.firstElementChild);
+document.body.appendChild($cloneCards);
+
+/******Modificando Elementos (Old Style)⬆️******/
+
+/******Modificando Elementos (Cool Style)⬆️******/
