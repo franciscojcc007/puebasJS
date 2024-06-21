0b52d138904845a2b8f0a614c5a2fc1ad0fed49d
franciscoj <frankmazta@gmail.com>
2024-06-21 10:11:03 -0400

    uso de preventDefault y stopPropagation

--- Evento/evento.js
+++ Evento/evento.js
@@ -1,21 +1,29 @@
-const $divsEventos = document.querySelectorAll(".eventos-flujo div");
+const $divsEventos = document.querySelectorAll(".eventos-flujo div"),
+  $linkEventos = document.querySelector(".eventos-flujo a");
 
 function flujoEventos(e) {
   console.log(
     `Hola te saluda ${this.className}, el click lo originó ${e.target.className}`
   );
+  e.stopPropagation();
 }
 
 console.log($divsEventos);
 
 $divsEventos.forEach((div) => {
   //Fase de burbuja
-  //div.addEventListener("click", flujoEventos);
+  div.addEventListener("click", flujoEventos);
   //div.addEventListener("click", flujoEventos, false);
   //Fase de captura
   //div.addEventListener("click", flujoEventos, true);
-  div.addEventListener("click", flujoEventos, {
-    capture: false,
-    once: true,
-  });
+  //div.addEventListener("click", flujoEventos, {
+  //capture: false,
+  //once: true,
+  //});
+});
+
+$linkEventos.addEventListener("click", (e) => {
+  alert("Hola soy tu amigo y docente digital... Jonathan MirCha");
+  e.preventDefault();
+  e.stopPropagation();
 });
