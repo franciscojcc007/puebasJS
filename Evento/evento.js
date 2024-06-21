dcb3aebfb518e781783094674c258b31cfc45a6c
franciscoj <frankmazta@gmail.com>
2024-06-21 09:28:02 -0400

    Flujo de Eventos (Burbuja y Captura)

--- Evento/evento.js
+++ Evento/evento.js
@@ -1,43 +1,21 @@
-function holaMundo() {
-  alert("Hola Mundo");
-  console.log(Event);
+const $divsEventos = document.querySelectorAll(".eventos-flujo div");
+
+function flujoEventos(e) {
+  console.log(
+    `Hola te saluda ${this.className}, el click lo originó ${e.target.className}`
+  );
 }
 
-function saludar(nombre = "Desconocid@") {
-  alert(`Hola ${nombre}`);
-  console.log(Event);
-}
+console.log($divsEventos);
 
-const $eventoSemantico = document.getElementById("evento-semantico"),
-  $eventoMultiple = document.getElementById("evento-multiple"),
-  $eventoRemover = document.getElementById("evento-remover");
-
-$eventoSemantico.onclick = holaMundo;
-$eventoSemantico.onclick = function (e) {
-  alert("Hola Mundo Manejador de Eventos Semántico");
-  console.log(e);
-  console.log(Event);
-};
-
-$eventoMultiple.addEventListener("click", holaMundo);
-$eventoMultiple.addEventListener("click", (e) => {
-  alert("Hola Mundo Manejador de Eventos Múltiple");
-  console.log(e);
-  console.log(e.type);
-  console.log(e.target);
-  console.log(Event);
+$divsEventos.forEach((div) => {
+  //Fase de burbuja
+  //div.addEventListener("click", flujoEventos);
+  //div.addEventListener("click", flujoEventos, false);
+  //Fase de captura
+  //div.addEventListener("click", flujoEventos, true);
+  div.addEventListener("click", flujoEventos, {
+    capture: false,
+    once: true,
+  });
 });
-$eventoMultiple.addEventListener("click", () => {
-  saludar();
-  saludar("Jon");
-});
-
-const removerDobleClick = (e) => {
-  alert(`Removiendo el evento de tipo ${e.type}`);
-  console.log(e);
-  $eventoRemover.removeEventListener("dblclick", removerDobleClick);
-  $eventoRemover.disabled = true;
-};
-
-$eventoRemover.addEventListener("dblclick", removerDobleClick);
-/******Eventos⬆️******/
