40255f3c43ab67b821920f3f5025164256ba0806
franciscoj <frankmazta@gmail.com>
2024-07-06 12:19:53 -0400

    completado

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,30 +1,36 @@
-(()=>{
-  const xhr = new XMLHttpRequest(),
-    $xhr = document.getElementById("xhr"),
-    $fragmet = document.createDocumentFragment();
+const xhr = new XMLHttpRequest(),
+  $xhr = document.getElementById("xhr"),
+  $fragment = document.createDocumentFragment();
 
-  xhr.addEventListener("readystatechange",(e)=>{
-    if(xhr.readyState !== 4)return
-    if(xhr.status >= 200 && xhr.status < 300){
-      // console.log(xhr);
-      // console.log("Exito")
-      // console.log(xhr.responseText)
-      let json = JSON.parse(xhr.responseText);
+xhr.addEventListener("readystatechange", (e) => {
+  if (xhr.readyState !== 4) return;
 
-      json.forEach((el) => {
-        const $li = document.createElement("li");
-        $li.innerHTML = `${el.name} ${el.email} ${el.phone}`;
-        $fragmet.appendChild($li);
-        
-      });
-      $xhr.appendChild($fragmet);
-    }else{
-      console.log("Error: " + xhr.status);
-    }
-  });
+  console.log(xhr);
 
+  if (xhr.status >= 200 && xhr.status < 300) {
+    console.log("éxito");
+    console.log(xhr.responseText);
+    //$xhr.innerHTML = xhr.responseText;
+    let json = JSON.parse(xhr.responseText);
+    console.log(json);
 
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+    json.forEach((el) => {
+      const $li = document.createElement("li");
+      $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+      $fragment.appendChild($li);
+    });
 
-  xhr.send();
-})();
+    $xhr.appendChild($fragment);
+  } else {
+    console.log("error");
+    let message = xhr.statusText || "Ocurrió un error";
+    $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+  }
+
+  console.log("Este mensaje cargará de cualquier forma");
+});
+
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+//xhr.open("GET", "assets/users.json");
+
+xhr.send();
