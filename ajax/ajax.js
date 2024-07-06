8e314a321db11592f07ada5e4e20ef2357ab3dfb
franciscoj <frankmazta@gmail.com>
2024-07-06 12:45:59 -0400

    uso de API Fetch + Async-Await

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -70,3 +70,35 @@
 //    );
 //  });
 })();
+(()=>{
+  const $fetchAsync = document.getElementById("fetch-async"),
+  $fragment = document.createDocumentFragment();
+
+async function getData() {
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+      json = await res.json();
+
+    console.log(res, json);
+
+    //if (!res.ok) throw new Error("Ocurrio un Error al solicitar los Datos");
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+    json.forEach((el) => {
+      const $li = document.createElement("li");
+      $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+      $fragment.appendChild($li);
+    });
+
+    $fetchAsync.appendChild($fragment);
+  } catch (err) {
+    console.log(err);
+    let message = err.statusText || "Ocurrió un error";
+    $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
+  } finally {
+    console.log("Esto se ejecutará independientemente del try... catch");
+  }
+}
+
+getData();
+})();
